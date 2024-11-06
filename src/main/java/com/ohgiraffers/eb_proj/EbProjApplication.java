33efb77ac67de6d5a3386319bb97b97a5dcb4811
rohdamin <rohdamin3@gmail.com>
2024-11-06 09:15:44 +0900

    Study AWS

---
+++ src/main/java/com/ohgiraffers/eb_proj/EbProjApplication.java
@@ -0,0 +1,19 @@
+package com.ohgiraffers.eb_proj;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class EbProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbProjApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
